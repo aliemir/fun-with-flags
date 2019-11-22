c2f113a2120dfa936ea9062a0b90132a84cb62ea
Ali Emir Şen <ali.sen@softtech.com.tr>
2019-11-22 16:54:46 +0300

    audit, button

--- src/App.js
+++ src/App.js
@@ -1,88 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import './App.scss';
+import React, { useEffect, useState } from 'react'
+import './App.scss'
 import Confetti from 'react-confetti'
-import { animated, useSpring } from 'react-spring';
-import { Header, Timer } from './components';
-import { shuffle, flags, useGameState, createQuestions, useWindowSize } from './utils';
+import { animated, useSpring } from 'react-spring'
+import { Header, Timer } from './components'
+import {
+  shuffle,
+  flags,
+  useGameState,
+  createQuestions,
+  useWindowSize,
+} from './utils'
 
 function App() {
-  const [installPrompt, setPrompt] = useState(null);
-  const [questions, setQuestions] = useState([...flags]);
-  const { width, height } = useWindowSize();
-  const [answerE, toggleAnswerE] = useState(0);
-  const [answerColor, setAnswerColor] = useState('rgb(22,168,22)');
-  const { bgAnim } = useSpring({ from: { bgAnim: 0 }, bgAnim: answerE ? 1 : 0, config: { duration: 750 } })
-  const correctColor = 'rgb(22,168,22)';
-  const wrongColor = 'rgb(239,84,84)';
+  const [installPrompt, setPrompt] = useState(null)
+  const [questions, setQuestions] = useState([...flags])
+  const { width, height } = useWindowSize()
+  const [answerE, toggleAnswerE] = useState(0)
+  const [answerColor, setAnswerColor] = useState('rgb(22,168,22)')
+  const { bgAnim } = useSpring({
+    from: { bgAnim: 0 },
+    bgAnim: answerE ? 1 : 0,
+    config: { duration: 750 },
+  })
+  const correctColor = 'rgb(22,168,22)'
+  const wrongColor = 'rgb(239,84,84)'
 
-  const [game, dispatch] = useGameState();
+  const [game, dispatch] = useGameState()
 
   useEffect(() => {
-    setQuestions(shuffle(createQuestions([...flags])));
-    dispatch({type:'INIT_GAME'});
-  }, [dispatch]);
-  
+    setQuestions(shuffle(createQuestions([...flags])))
+    dispatch({ type: 'INIT_GAME' })
+  }, [dispatch])
+
   useEffect(() => {
-    window.addEventListener('beforeinstallprompt', (e) => {
+    window.addEventListener('beforeinstallprompt', e => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
+      e.preventDefault()
       // Stash the event so it can be triggered later.
-      setPrompt(e);
-    });
-    
-  }, []);
+      setPrompt(e)
+    })
+  }, [])
 
   async function installApp() {
     if (installPrompt) {
-      installPrompt.prompt();
-      installPrompt.userChoice.then(function(choiceResult){
-  
+      installPrompt.prompt()
+      installPrompt.userChoice.then(function(choiceResult) {
         if (choiceResult.outcome === 'accepted') {
-        console.log('Your PWA has been installed');
-      } else {
-        console.log('User chose to not install your PWA');
-      }
-  
-        setPrompt(null);
-  
-      });
-  
-  
+          console.log('Your PWA has been installed')
+        } else {
+          console.log('User chose to not install your PWA')
+        }
+
+        setPrompt(null)
+      })
     }
   }
 
   async function shareApp(title, text, url) {
     if (navigator.share) {
-      navigator.share({
-        title: 'Fun with Flags!',
-        text: `Check out this new addictive game! My highest score is ${game.stats.highScore}`,
-        url: "https://funflags.now.sh"
-      }).then(() => {
-        console.log('Thanks for sharing!');
-      })
-      .catch(err => {
-        console.log(`Couldn't share because of`, err.message);
-      });
+      navigator
+        .share({
+          title: 'Fun with Flags!',
+          text: `Check out this new addictive game! My highest score is ${game.stats.highScore}`,
+          url: 'https://funflags.now.sh',
+        })
+        .then(() => {
+          console.log('Thanks for sharing!')
+        })
+        .catch(err => {
+          console.log(`Couldn't share because of`, err.message)
+        })
     } else {
-      console.log('web share not supported');
+      console.log('web share not supported')
     }
   }
 
   const startGame = () => {
-    dispatch({type: 'START_GAME'});
+    dispatch({ type: 'START_GAME' })
   }
   const showStats = () => {
-    dispatch({ type: 'STATS' });
+    dispatch({ type: 'STATS' })
   }
   const pauseGame = () => {
-    dispatch({type: 'PAUSE_GAME'});
+    dispatch({ type: 'PAUSE_GAME' })
   }
   const unpauseGame = () => {
-    dispatch({type: 'UNPAUSE_GAME'});
+    dispatch({ type: 'UNPAUSE_GAME' })
   }
   const resetGame = () => {
-    setQuestions(shuffle(createQuestions([...flags])));
-    dispatch({type: 'RESET_GAME'});
+    setQuestions(shuffle(createQuestions([...flags])))
+    dispatch({ type: 'RESET_GAME' })
   }
   const shareGame = () => {
     shareApp('Fun With Flags', 'https://funflags.now.sh', '')
@@ -90,137 +97,185 @@ function App() {
   const shareScore = () => {
     shareApp('Fun With Flags', 'https://funflags.now.sh', '')
   }
-  const checkAnswer = (e) => {
-    const answer = e;
+  const checkAnswer = e => {
+    const answer = e
     if (answer === true) {
-      setAnswerColor(correctColor);
-      toggleAnswerE(!answerE);
-      dispatch({type: 'CORRECT'});
+      setAnswerColor(correctColor)
+      toggleAnswerE(!answerE)
+      dispatch({ type: 'CORRECT' })
     } else {
-      setAnswerColor(wrongColor);
-      toggleAnswerE(!answerE);
-      dispatch({ type: 'WRONG' });
+      setAnswerColor(wrongColor)
+      toggleAnswerE(!answerE)
+      dispatch({ type: 'WRONG' })
     }
   }
 
-  const renderQuestion = (qIndex) => {
-    const qItem = questions[qIndex];
+  const renderQuestion = qIndex => {
+    const qItem = questions[qIndex]
     return (
       <div style={{ textAlign: 'center' }}>
-        <div className="flag">{qItem.flag}</div>
-        <div className="choices">
+        <div className='flag'>{qItem.flag}</div>
+        <div className='choices'>
           {qItem.choices.map(choice => {
-            return (<button type="button" key={choice.name} onClick={() => checkAnswer(choice.correct)}>{choice.name}</button>)
+            return (
+              <button
+                type='button'
+                key={choice.name}
+                onClick={() => checkAnswer(choice.correct)}
+              >
+                {choice.name}
+              </button>
+            )
           })}
         </div>
       </div>
-    );
+    )
   }
 
   const onPlayRender = () => {
     return (
-      <div className="onPlay">
+      <div className='onPlay'>
         <div style={{ textAlign: 'center' }}>
           {renderQuestion(game.counter.question)}
         </div>
       </div>
     )
-  };
+  }
 
   const onStatsRender = () => {
-    const total = game.stats.correctCount + game.stats.wrongCount;
+    const total = game.stats.correctCount + game.stats.wrongCount
+    const correctRate = isNaN(game.stats.correctCount / total)
+      ? 0
+      : game.stats.correctCount / total
+    const wrongCount = isNaN(game.stats.wrongCount / total)
+      ? 0
+      : game.stats.wrongCount / total
     return (
-      <div className="onStats" style={{textAlign: 'left'}}>
+      <div className='onStats' style={{ textAlign: 'left' }}>
         <div>HIGH SCORE : {game.stats.highScore}</div>
         <div>TIMES PLAYED : {game.stats.timesPlayed}</div>
-        <div>CORRECT : {(game.stats.correctCount / total * 100).toFixed(0)}%</div>
-        <div>WRONG : {(game.stats.wrongCount / total * 100).toFixed(0)}%</div>
+        <div>CORRECT : {(correctRate * 100).toFixed(0)}%</div>
+        <div>WRONG : {(wrongCount * 100).toFixed(0)}%</div>
         <span>...</span>
-        <button type="button" onClick={() => resetGame()}>GO BACK</button>
+        <button type='button' onClick={() => resetGame()}>
+          GO BACK
+        </button>
       </div>
     )
   }
 
   const onPauseRender = () => {
     return (
-      <div className="onPause">
+      <div className='onPause'>
         PAUSED!
         <span>...</span>
-        <button type="button" onClick={() => unpauseGame()}>RESUME</button>
-        <button type="button" onClick={() => resetGame()}>RESTART</button>
+        <button type='button' onClick={() => unpauseGame()}>
+          RESUME
+        </button>
+        <button type='button' onClick={() => resetGame()}>
+          RESTART
+        </button>
       </div>
     )
-  };
+  }
 
   const onOverRender = () => {
     return (
-      <div className="onOver">
-        {game.gameOverType === 'newHighScore' ? <Confetti width={width} height={height} /> : ''}
-        {game.gameOverType === 'newHighScore' ? 'NEW HIGH SCORE!' : 'GAME OVER!'}
+      <div className='onOver'>
+        {game.gameOverType === 'newHighScore' ? (
+          <Confetti width={width} height={height} />
+        ) : (
+          ''
+        )}
+        {game.gameOverType === 'newHighScore'
+          ? 'NEW HIGH SCORE!'
+          : 'GAME OVER!'}
         <span>...</span>
-        <button type="button" onClick={() => resetGame()}>PLAY AGAIN</button>
-        <button type="button" onClick={() => shareScore()}>SHARE WITH FRIENDS</button>
+        <button type='button' onClick={() => resetGame()}>
+          PLAY AGAIN
+        </button>
+        <button type='button' onClick={() => shareScore()}>
+          SHARE WITH FRIENDS
+        </button>
       </div>
     )
   }
 
   const onStartRender = () => {
     return (
-      <div className="onStart">
-        <button type="button" onClick={() => startGame()}>START GAME</button>
-        <button type="button" onClick={() => showStats()}>STATISTICS</button>
-        <button type="button" onClick={() => shareGame()}>CHALLENGE YOUR FRIENDS</button>
-        <button type="button" className="addToHomeScreen"onClick={() => installApp()}>DOWNLOAD THE APP</button>
+      <div className='onStart'>
+        <button
+          className='start-game-button'
+          type='button'
+          onClick={() => startGame()}
+        >
+          START GAME
+        </button>
+        <button type='button' onClick={() => showStats()}>
+          STATISTICS
+        </button>
+        <button type='button' onClick={() => shareGame()}>
+          CHALLENGE YOUR FRIENDS
+        </button>
+        <button
+          type='button'
+          className='addToHomeScreen'
+          onClick={() => installApp()}
+        >
+          DOWNLOAD THE APP
+        </button>
       </div>
     )
   }
 
-  const renderPage = (status) => {
+  const renderPage = status => {
     switch (status) {
       case 'zero':
-        return onStartRender();
+        return onStartRender()
       case 'active':
-        return onPlayRender();
+        return onPlayRender()
       case 'over':
-        return onOverRender();
+        return onOverRender()
       case 'paused':
-        return onPauseRender();
+        return onPauseRender()
       case 'stats':
-        return onStatsRender();
+        return onStatsRender()
       default:
-        return (<div>UNHANDLED_STATUS</div>)
+        return <div>UNHANDLED_STATUS</div>
     }
   }
 
-  if(game) {
+  if (game) {
     return (
-      <animated.div className="App" style={{height, backgroundColor: bgAnim
-        .interpolate({
-          range: [0, 0.5, 1],
-          output: ['white', answerColor, 'white']
-        })}}>
-        <Header scores={game.score} onCloseClick={() => {
-          if (game.gameStatus === 'paused') {
-            unpauseGame()
-          } else if(game.gameStatus === 'active') {
-            pauseGame()
-          } else {
-            resetGame()
-          }
-        }} />
-        <Timer time={game.time} effect={game.timeEffect}/>
-        <div className="container">
-          {renderPage(game.gameStatus)}
-        </div>
-        <div className="ads">
-          advertisement
-        </div>
+      <animated.div
+        className='App'
+        style={{
+          height,
+          backgroundColor: bgAnim.interpolate({
+            range: [0, 0.5, 1],
+            output: ['white', answerColor, 'white'],
+          }),
+        }}
+      >
+        <Header
+          scores={game.score}
+          onCloseClick={() => {
+            if (game.gameStatus === 'paused') {
+              unpauseGame()
+            } else if (game.gameStatus === 'active') {
+              pauseGame()
+            } else {
+              resetGame()
+            }
+          }}
+        />
+        <Timer time={game.time} effect={game.timeEffect} />
+        <div className='container'>{renderPage(game.gameStatus)}</div>
+        <div className='ads'>advertisement</div>
       </animated.div>
-    );
-  } 
-    return (<div className="Loading">...</div>);
-  
-
+    )
+  }
+  return <div className='Loading'>...</div>
 }
 
-export default App;
+export default App
--- src/utils/useGameState.js
+++ src/utils/useGameState.js
@@ -1,119 +1,166 @@
-import { useReducer } from 'react';
-import { useLocalStorage, useInterval, calcMultiplier } from ".";
+import { useReducer } from 'react'
+import { useLocalStorage, useInterval, calcMultiplier } from '.'
 
 const useGameState = () => {
-  const [localData, setLocalData] = useLocalStorage('gameData', JSON.stringify({highScore:0, timesPlayed: 0, correctCount: 0, wrongCount: 0}));
+  const [localData, setLocalData] = useLocalStorage(
+    'gameData',
+    JSON.stringify({
+      highScore: 0,
+      timesPlayed: 0,
+      correctCount: 0,
+      wrongCount: 0,
+    }),
+  )
 
   const [game, dispatch] = useReducer((state, action) => {
-    const { highScore, timesPlayed, correctCount, wrongCount } = JSON.parse(localData);
-    switch(action.type) {
-      default :
-        console.log('UNHANDLED_TYPE', action.type);
-      break;
-      case 'DECREMENT_TIME': 
+    const { highScore, timesPlayed, correctCount, wrongCount } = JSON.parse(
+      localData,
+    )
+    switch (action.type) {
+      default:
+        console.log('UNHANDLED_TYPE', action.type)
+        break
+      case 'DECREMENT_TIME':
         return {
           ...state,
-          time: state.time -2
+          time: state.time - 2,
         }
       case 'INIT_GAME':
         return {
-          time: 155,
+          time: 150,
           isRunning: false,
           gameStatus: 'zero',
-          timeEffect: {state: 0, color: {r: 250, g: 215, b: 68}},
-          score: {currentScore: 0, scoreMultiplier: 1, highScore, streak: 0},
+          timeEffect: { state: 0, color: { r: 250, g: 215, b: 68 } },
+          score: { currentScore: 0, scoreMultiplier: 1, highScore, streak: 0 },
           counter: { question: 0, correct: 0, wrong: 0 },
-          stats: {highScore,timesPlayed,correctCount,wrongCount}
-        };
-      case 'START_GAME': 
+          stats: { highScore, timesPlayed, correctCount, wrongCount },
+        }
+      case 'START_GAME':
         return {
           ...state,
           isRunning: true,
-          gameStatus: 'active'
+          gameStatus: 'active',
         }
       case 'STATS':
         return {
           ...state,
           isRunning: false,
-          gameStatus: 'stats'
+          gameStatus: 'stats',
         }
       case 'SHARE':
         return {
           ...state,
           isRunning: false,
-          gameStatus: 'share'
+          gameStatus: 'share',
         }
       case 'RESET_GAME':
-          return {
-            time: 155,
-            isRunning: false,
-            gameStatus: 'zero',
-            timeEffect: {state: 0, color: {r: 250, g: 215, b: 68}},
-            score: {currentScore: 0, scoreMultiplier: 1, highScore, streak: 0},
-            counter: { question: 0, correct: 0, wrong: 0 },
-            stats: {highScore,timesPlayed,correctCount,wrongCount}
-          };
+        return {
+          time: 155,
+          isRunning: false,
+          gameStatus: 'zero',
+          timeEffect: { state: 0, color: { r: 250, g: 215, b: 68 } },
+          score: { currentScore: 0, scoreMultiplier: 1, highScore, streak: 0 },
+          counter: { question: 0, correct: 0, wrong: 0 },
+          stats: { highScore, timesPlayed, correctCount, wrongCount },
+        }
       case 'PAUSE_GAME':
+        return {
+          ...state,
+          isRunning: false,
+          gameStatus: 'paused',
+        }
+      case 'UNPAUSE_GAME':
+        return {
+          ...state,
+          isRunning: true,
+          gameStatus: 'active',
+        }
+      case 'GAME_OVER':
+        if (highScore < state.score.currentScore) {
+          setLocalData(
+            JSON.stringify({
+              highScore: state.score.currentScore,
+              timesPlayed: timesPlayed + 1,
+              correctCount: correctCount + state.counter.correct,
+              wrongCount: wrongCount + state.counter.wrong,
+            }),
+          )
           return {
             ...state,
             isRunning: false,
-            gameStatus: 'paused'
+            gameStatus: 'over',
+            gameOverType: 'newHighScore',
           }
-      case 'UNPAUSE_GAME':
-          return {
-            ...state,
-            isRunning: true,
-            gameStatus: 'active'
-          }
-      case 'GAME_OVER':
-        
-          if(highScore < state.score.currentScore) {
-            setLocalData(JSON.stringify({highScore:state.score.currentScore, timesPlayed: timesPlayed+1, correctCount: correctCount+state.counter.correct, wrongCount: wrongCount+state.counter.wrong}));
-            return {
-              ...state,
-              isRunning: false,
-              gameStatus: 'over',
-              gameOverType: 'newHighScore'
-            };
-        } 
-        setLocalData(JSON.stringify({highScore:state.score.highScore, timesPlayed: timesPlayed+1, correctCount: correctCount+state.counter.correct, wrongCount: wrongCount+state.counter.wrong}));
-            return {
-              ...state,
-              isRunning: false,
-              gameStatus: 'over',
-              gameOverType: 'timeOver'
-            }
-        
+        }
+        setLocalData(
+          JSON.stringify({
+            highScore: state.score.highScore,
+            timesPlayed: timesPlayed + 1,
+            correctCount: correctCount + state.counter.correct,
+            wrongCount: wrongCount + state.counter.wrong,
+          }),
+        )
+        return {
+          ...state,
+          isRunning: false,
+          gameStatus: 'over',
+          gameOverType: 'timeOver',
+        }
+
       case 'CORRECT':
         return {
           ...state,
-          time: (state.time + 20 > 150 ? 150 : state.time + 20),
-          timeEffect: {state: !state.timeEffect.state, color: {r:22, g: 168, b: 22}},
-          score: { ...state.score, currentScore: state.score.currentScore + (10 * state.score.scoreMultiplier), streak: state.score.streak + 1, scoreMultiplier: calcMultiplier(state.score.streak, state.score.scoreMultiplier) },
-          counter: {question: state.counter.question+1, correct: state.counter.correct+1, wrong: state.counter.wrong}
+          time: state.time + 20 > 150 ? 150 : state.time + 20,
+          timeEffect: {
+            state: !state.timeEffect.state,
+            color: { r: 22, g: 168, b: 22 },
+          },
+          score: {
+            ...state.score,
+            currentScore:
+              state.score.currentScore + 10 * state.score.scoreMultiplier,
+            streak: state.score.streak + 1,
+            scoreMultiplier: calcMultiplier(
+              state.score.streak,
+              state.score.scoreMultiplier,
+            ),
+          },
+          counter: {
+            question: state.counter.question + 1,
+            correct: state.counter.correct + 1,
+            wrong: state.counter.wrong,
+          },
         }
       case 'WRONG':
         return {
           ...state,
           time: state.time - 25,
-          timeEffect: {state: !state.timeEffect.state, color: {r: 239, g: 84, b: 84}},
+          timeEffect: {
+            state: !state.timeEffect.state,
+            color: { r: 239, g: 84, b: 84 },
+          },
           score: { ...state.score, scoreMultiplier: 1, streak: 0 },
-          counter: {question: state.counter.question+1, correct: state.counter.correct, wrong: state.counter.wrong+1}
+          counter: {
+            question: state.counter.question + 1,
+            correct: state.counter.correct,
+            wrong: state.counter.wrong + 1,
+          },
         }
     }
-  });
+  })
 
-    useInterval(() => {
-      if(game.time <= 0 ) {
-        dispatch({type: 'GAME_OVER'});
+  useInterval(
+    () => {
+      if (game.time <= 0) {
+        dispatch({ type: 'GAME_OVER' })
       } else {
-        dispatch({type: 'DECREMENT_TIME'})
+        dispatch({ type: 'DECREMENT_TIME' })
       }
-    },game && game.isRunning ? 200 : null);
+    },
+    game && game.isRunning ? 200 : null,
+  )
 
+  return [game, dispatch]
+}
 
-  
-  return [game, dispatch];
-};
-
-export default useGameState;
+export default useGameState
